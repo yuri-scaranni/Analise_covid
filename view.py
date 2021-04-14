5d9a67310b1857035b26c80778eeba1938381681
Yuri Scaranni <yuri.scaranni@tdwbi.com.br>
2021-04-14 18:03:30 -0300

    edit view

--- view.py
+++ view.py
@@ -6,32 +6,57 @@ import matplotlib.pyplot as plt
 SA - SOUTH AMÉRICA
 """
 
-def sa_mortes_total_por_mes():
-    dataframe = pd.read_csv('download/covid19_SA.csv', sep=',')
-    dataframe['data'] = pd.to_datetime(dataframe['data']).apply(lambda x: x.strftime('%Y-%m'))
-    dataframe = dataframe.query('pais == ["Brazil", "Argentina", "Paraguay", "Uruguay", "Bolivia"]')
-    sns.set_theme(style="whitegrid")
-    sns.lineplot(data=dataframe, x='data', y='mortes_totais', hue='pais', palette="flare")
+
+def sa_mortes_total():
+    df = pd.read_csv('download/covid19_SA.csv', sep=',')
+    df['data'] = pd.to_datetime(df['data']).apply(lambda x: x.strftime('%Y-%m'))
+    df = df.query('pais == ["Brazil", '
+                  '"Argentina", '
+                  '"Paraguay", '
+                  '"Uruguay", '
+                  '"Bolivia"]')
+    sns.set_theme(style="darkgrid")
+    sns.lineplot(data=df,
+                 x='data',
+                 y='mortes_totais',
+                 hue='pais')
     plt.xlabel("Ano-Mês")
     plt.ylabel("Mortes Totais")
     plt.title("Acumulado de mortes ao longo do tempo.")
     plt.show()
 
 
-def sa_casos_total_por_mes():
-    dataframe = pd.read_csv('download/covid19_SA.csv', sep=',')
-    dataframe['data'] = pd.to_datetime(dataframe['data']).apply(lambda x: x.strftime('%Y-%m'))
-    dataframe = dataframe.query('pais == ["Brazil", "Argentina", "Paraguay", "Uruguay", "Bolivia"]')
-    sns.lineplot(data=dataframe, x='data', y='casos_totais', hue='pais')
+def sa_casos_total():
+    df = pd.read_csv('download/covid19_SA.csv', sep=',')
+    df['data'] = pd.to_datetime(df['data']).apply(lambda x: x.strftime('%Y-%m'))
+    df = df.query('pais == ["Brazil", '
+                  '"Argentina", '
+                  '"Paraguay", '
+                  '"Uruguay", '
+                  '"Bolivia"]')
+    sns.lineplot(data=df, x='data', y='casos_totais', hue='pais')
     plt.xlabel("Ano-Mês")
     plt.ylabel("Casos Totais")
     plt.title("Acumulado de casos ao longo do tempo.")
     plt.show()
 
+sa_casos_total()
 
 def sa_mortes_por_milhao():
-    dataframe = pd.read_csv('download/covid19_SA.csv', sep=',')
-    dataframe['data'] = pd.to_datetime(dataframe['data']).apply(lambda x: x.strftime('%Y-%m'))
-    dataframe = dataframe.query('pais == ["Brazil", "Argentina", "Paraguay", "Uruguay", "Bolivia"]')
-    sns.lineplot(data=dataframe, x='data', y='total_mortes_por_milhao', hue='pais')
+    df = pd.read_csv('download/covid19_SA.csv', sep=',')
+    df['data'] = pd.to_datetime(df['data']).apply(lambda x: x.strftime('%Y-%m'))
+    df = df.query('pais == ["Brazil", '
+                  '"Argentina", '
+                  '"Paraguay", '
+                  '"Uruguay", '
+                  '"Bolivia"]')
+    sns.lineplot(data=df,
+                 x='data',
+                 y='total_mortes_por_milhao',
+                 hue='pais')
+    plt.xlabel("Ano-Mês")
+    plt.ylabel("Mortes")
+    plt.title("Número de mortes por milhão de habitantes.")
     plt.show()
+
+
